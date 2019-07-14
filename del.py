b842da826ffbb33aad6c9876cd3a6f9883891d38
mochammadfariz <mochammadfariz11@gmail.com>
2019-07-14 11:49:40 +0700

    memfilter atribut

--- del.py
+++ del.py
@@ -6,4 +6,6 @@ from sklearn.utils import shuffle
 
 
 data = pd.read_csv("student-mat.csv",sep=";")
-
+print(data.head())
+data = data[["G1","G2","G3","studytime","failures","absences"]]
+print(data.head())
