d0f402d218d96ab3363cd2c834e801dc77db914b
mochammadfariz <mochammadfariz11@gmail.com>
2019-07-16 17:45:24 +0700

    split 0.1

--- del.py
+++ del.py
@@ -12,3 +12,6 @@ data = data[["G1","G2","G3","studytime","failures","absences"]]
 predict = "G3"
 
 X = np.array(data.drop([predict],1))
+y = np.array(data[predict])
+
+x_train, y_train,x_test,y_test = sklearn.model_selection.train_test_split(x,y,test_size=0.1)
